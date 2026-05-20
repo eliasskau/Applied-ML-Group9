939f890e491b113bbe0b3e7aa65feacfd8b1b6fc
eliasskau <eliasskau@gmail.com>
2026-05-20 19:38:52 +0200

    loss function,optimizer and training

--- project/models/CNN_model.py
+++ project/models/CNN_model.py
@@ -65,3 +65,29 @@ class Dog_Model(nn.Module):
         x = self.dropout(x)
         x = self.fc2(x)
         return x
+
+
+model = Dog_Model(num_classes)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
