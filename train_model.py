e0944e6cf8b7f59b6efcc050f7eea34018cbfc56
eliasskau <eliasskau@gmail.com>
2026-05-22 09:05:35 +0200

    added if name == __main__

--- train_model.py
+++ train_model.py
@@ -27,50 +27,52 @@ transform = transforms.Compose([
     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 ])
 
-full_dataset = ImageFolder(root=DATA_DIR, transform=transform)
+if __name__ == '__main__':
+    full_dataset = ImageFolder(root=DATA_DIR, transform=transform)
 
-train_size = int(0.8 * len(full_dataset))
-test_size = len(full_dataset) - train_size
-train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
+    train_size = int(0.8 * len(full_dataset))
+    test_size = len(full_dataset) - train_size
+    train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=2)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
 
-model = Dog_Model(num_classes)
+    model = Dog_Model(num_classes)
 
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-for epoch in range(num_epochs):
-    running_loss = 0.0
-    for i, data in enumerate(train_loader, 0):
-        inputs, labels = data
 
-        optimizer.zero_grad()
+    for epoch in range(num_epochs):
+        running_loss = 0.0
+        for i, data in enumerate(train_loader, 0):
+            inputs, labels = data
 
-        outputs = model(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+            optimizer.zero_grad()
 
-        running_loss += loss.item()
-        if i % 50 == 49:
-            avg_loss = running_loss / 50
-            print(f'Avg loss over last 50 batches: {avg_loss:.3f}')
-            running_loss = 0.0
-
-    model.eval()
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for inputs, labels in test_loader:
             outputs = model(inputs)
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-    accuracy = 100 * correct / total
-    print(f'Test accuracy: {accuracy:.2f}%\n')
+            running_loss += loss.item()
+            if i % 50 == 49:
+                avg_loss = running_loss / 50
+                print(f'Avg loss over last 50 batches: {avg_loss:.3f}')
+                running_loss = 0.0
 
-torch.save(model.state_dict(), 'models/dog_model.pth')
-print(f'Model saved to models/dog_model.pth')
+        model.eval()
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for inputs, labels in test_loader:
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        accuracy = 100 * correct / total
+        print(f'Test accuracy: {accuracy:.2f}%\n')
+
+    torch.save(model.state_dict(), 'models/dog_model.pth')
+    print(f'Model saved to models/dog_model.pth')
