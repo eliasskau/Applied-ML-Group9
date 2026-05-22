8aa401494b23b64c09262f05f98350202aa99f5a
eliasskau <eliasskau@gmail.com>
2026-05-22 14:05:09 +0200

    removed MPS device option due to unsupported ops, falls back to CPU

--- train_model.py
+++ train_model.py
@@ -21,7 +21,7 @@ learning_rate= 0.001
 num_epochs = 30
 batch_size = 32
 
-device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device: {device}")
 
 train_transform = transforms.Compose([
