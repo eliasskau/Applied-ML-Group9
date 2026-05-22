3bbbed90d2dc7b90dafe3083c37b28a1e643d7e7
eliasskau <eliasskau@gmail.com>
2026-05-22 08:30:50 +0200

    added train_model.py and moved training code there

---
+++ train_model.py
@@ -0,0 +1,61 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+# data
+
+DATA_DIR = "data/images"
+IMG_SIZE = 384
+classes = ["alert", "angry", "frown", "happy", "relax"]
+num_classes = len(classes)
+
+
+#hyper param
+learning_rate= 0.001
+num_epochs = 10
+batch_size = 32
+
+transform = transforms.Compose([
+    transforms.Resize((IMG_SIZE, IMG_SIZE)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+full_dataset = ImageFolder(root=DATA_DIR, transform=transform)
+
+train_size = int(0.8 * len(full_dataset))
+test_size = len(full_dataset) - train_size
+train_dataset, test_dataset = torch.utils.data.random_split(full_dataset, [train_size, test_size])
+
+train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, num_workers=2)
+test_loader = DataLoader(test_dataset, batch_size=4, shuffle=False, num_workers=2)
+
+model = Dog_Model(num_classes)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 2000 == 1999:
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
